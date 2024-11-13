3d4ed095048a690a0f97658d281c957330e764a7
HuyNguyen6969 <binkhongz069@gmail.com>
2024-11-13 18:51:56 -0500

    Implemented Lab 10

--- whackamole.py
+++ whackamole.py
@@ -27,8 +27,9 @@ def main():
             x = random.randrange(0, GRID_WIDTH) * GRID_SIZE
             y = random.randrange(0, GRID_HEIGHT) * GRID_SIZE
             return (x, y)
-
-        # Main game loop
+        
+        
+        #Main game
         while running:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -37,21 +38,18 @@ def main():
                     mouse_x, mouse_y = event.pos
                     mole_x, mole_y = mole_position
                     if mole_x <= mouse_x < mole_x + GRID_SIZE and mole_y <= mouse_y < mole_y + GRID_SIZE:
-                        mole_position = move_mole_randomly()  # Move the mole to a new random position
+                        mole_position = move_mole_randomly()  # Move mole to new rand position
 
-            # Clear the screen and draw the grid
             screen.fill("light green")
             draw_grid()
 
-            # Draw the mole at its current position
+            # Draw the mole at current position
             screen.blit(mole_image, mole_image.get_rect(topleft=mole_position))
 
-            # Update the display and control the frame rate
             pygame.display.flip()
             clock.tick(60)
     finally:
         pygame.quit()
 
-# Run the game
 if __name__ == "__main__":
     main()
